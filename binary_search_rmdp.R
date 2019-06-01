8ca1e03a8a6f3fb8d3974a2e1b89aad3de05513c
Waldyn <martinwg@miamioh.edu>
2019-05-31 22:43:50 -0400

    RMDP fix

--- binary_search_rmdp.R
+++ binary_search_rmdp.R
@@ -1,9 +1,13 @@
 setwd("/home/martinwg/OCP")
-a = 12
-write.csv(a, "a.csv")
-generate_data <- function(obsin, obsout, p, s, dist, seed) 
+library(doParallel)
+
+generate_data <- function(obsin, obsout, p, s, dist, sim, h1) 
 {
-  set.seed(seed)
+  #set.seed(seed)
+  FP <- c()
+  registerDoParallel(cores=8)
+  iter<-foreach (i=1:sim) %dopar%
+  {
   if (dist=="Normal") {
     gennorm = function(obsin, p, s, obsout = 0, deltap = 0.023){
       require(mvtnorm)
@@ -76,83 +80,92 @@ generate_data <- function(obsin, obsout, p, s, dist, seed)
     }
     data <- gent(obsin, p, s, obsout)
   }
-  return(data)
-}
-
-#b = head(data)
-#write.csv(b, "b.csv")
-
-RMDP<-function(y,alpha=0.05,itertime=100)   ############RMDP procedure
-{
-  n<-nrow(y)
-  p<-ncol(y)
-  h<-round(n/2)+1
-  init_h=2
-  delta=alpha/2
-  bestdet=0
-  jvec<-array(0,dim=c(n,1))
-  for(A in 1:itertime)
-  {
-    id=sample(n,init_h)
-    ny=y[id,]
-    mu_t=apply(ny,2,mean)
-    var_t=apply(ny,2,var) 
-    dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
-    crit=10
-    l=0
-    while(crit!=0&l<=15)
-    {	l=l+1
-    ivec<-array(0,dim=c(n,1))
-    dist_perm<-order(dist)
-    ivec[dist_perm[1:h]]=1
-    crit=sum(abs(ivec-jvec))
-    jvec=ivec
-    newy=y[dist_perm[1:h],]
-    mu_t=apply(newy,2,mean)
-    var_t=apply(newy,2,var) 
-    dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
+    RMDP<-function(y,alpha=0.05,itertime=100)   ############RMDP procedure
+    {
+      n<-nrow(y)
+      p<-ncol(y)
+      h<-round(n/2)+1
+      init_h=2
+      delta=alpha/2
+      bestdet=0
+      jvec<-array(0,dim=c(n,1))
+      for(A in 1:itertime)
+      {
+        id=sample(n,init_h)
+        ny=y[id,]
+        mu_t=apply(ny,2,mean)
+        var_t=apply(ny,2,var) 
+        dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
+        crit=10
+        l=0
+        while(crit!=0&l<=15)
+        {	l=l+1
+        ivec<-array(0,dim=c(n,1))
+        dist_perm<-order(dist)
+        ivec[dist_perm[1:h]]=1
+        crit=sum(abs(ivec-jvec))
+        jvec=ivec
+        newy=y[dist_perm[1:h],]
+        mu_t=apply(newy,2,mean)
+        var_t=apply(newy,2,var) 
+        dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
+        }
+        tempdet=prod(var_t)
+        if(bestdet==0|tempdet<bestdet) 
+        {bestdet=tempdet
+        final_vec=jvec
+        }
+      }
+      submcd<-seq(1,n)[final_vec!=0]
+      ##Estimated Mean and Variance
+      mu_t=apply(y[submcd,],2,mean)
+      var_t=apply(y[submcd,],2,var) 
+      ##Squared Disatances
+      dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
+      ##Squared Distances rescaled
+      dist=dist*p/median(dist)
+      ###Correlation matrix of the minimum volume subset
+      ER=cor(y[submcd,])
+      ##Squared correlation matrix of the minimum volume subset
+      ER=ER%*%ER
+      ##Calculating argument for inside of the Z{}function in eq (9)
+      tr2_h=sum(diag(ER))
+      tr2=tr2_h-p^2/h
+      cpn_0=1+(tr2_h)/p^1.5
+      ##Calculating weight from eq (9) w0 returns a TRUE or FALSE for each value in Sample
+      w0=(dist-p)/sqrt(2*tr2*cpn_0)<qnorm(1-delta)
+      nw=sum(w0)
+      ##Data is subset based on TRUE w0 values
+      sub=seq(1,n)[w0]	
+      mu_t=apply(y[sub,],2,mean)
+      var_t=apply(y[sub,],2,var) 
+      ER=cor(y[sub,])
+      ER=ER%*%ER
+      tr2_h=sum(diag(ER))
+      tr2=tr2_h-p^2/nw
+      dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
+      scale=1+1/sqrt(2*pi)*exp(-qnorm(1-delta)^2/2)/(1-delta)*sqrt(2*tr2)/p
+      dist=dist/scale
+      cpn_1=1+(tr2_h)/p^1.5
+      w1=(dist-p)/sqrt(2*tr2*cpn_1)<qnorm(1-alpha)
+      list(w1=w1)
+      return(dis_rmdp = (dist-p)/sqrt(2*tr2*cpn_1))
     }
-    tempdet=prod(var_t)
-    if(bestdet==0|tempdet<bestdet) 
-    {bestdet=tempdet
-    final_vec=jvec
-    }
+    rmdp_res = RMDP(data$data)
+    FP[i] = sum(rmdp_res > h1) / obsin
   }
-  submcd<-seq(1,n)[final_vec!=0]
-  ##Estimated Mean and Variance
-  mu_t=apply(y[submcd,],2,mean)
-  var_t=apply(y[submcd,],2,var) 
-  ##Squared Disatances
-  dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
-  ##Squared Distances rescaled
-  dist=dist*p/median(dist)
-  ###Correlation matrix of the minimum volume subset
-  ER=cor(y[submcd,])
-  ##Squared correlation matrix of the minimum volume subset
-  ER=ER%*%ER
-  ##Calculating argument for inside of the Z{}function in eq (9)
-  tr2_h=sum(diag(ER))
-  tr2=tr2_h-p^2/h
-  cpn_0=1+(tr2_h)/p^1.5
-  ##Calculating weight from eq (9) w0 returns a TRUE or FALSE for each value in Sample
-  w0=(dist-p)/sqrt(2*tr2*cpn_0)<qnorm(1-delta)
-  nw=sum(w0)
-  ##Data is subset based on TRUE w0 values
-  sub=seq(1,n)[w0]	
-  mu_t=apply(y[sub,],2,mean)
-  var_t=apply(y[sub,],2,var) 
-  ER=cor(y[sub,])
-  ER=ER%*%ER
-  tr2_h=sum(diag(ER))
-  tr2=tr2_h-p^2/nw
-  dist<-apply((t((t(y)-mu_t)/var_t^0.5))^2,1,sum)
-  scale=1+1/sqrt(2*pi)*exp(-qnorm(1-delta)^2/2)/(1-delta)*sqrt(2*tr2)/p
-  dist=dist/scale
-  cpn_1=1+(tr2_h)/p^1.5
-  w1=(dist-p)/sqrt(2*tr2*cpn_1)<qnorm(1-alpha)
-  list(w1=w1)
-  return(dis_rmdp = (dist-p)/sqrt(2*tr2*cpn_1))
+  iters<-do.call(rbind, iter)
+  
+  #ji <- ji + 1
+  #avg_FP[ji] <- mean(FP)
+  #avg_FP[ji] <- mean(iters)
+  #}
+  #proc.time() - ptm
+  #avg_FP <- data.frame(avg_FP)
+  #rownames(avg_FP) <- h1
+  return(mean(iters))
 }
+
 
 
 
@@ -164,7 +177,10 @@ hBinarySearch <- function(l_pred_bound, u_pred_bound, pred_h, obs, p, dist = "No
   u <- u_pred_bound
   m <- pred_h
   #m <- (l_pred_bound + u_pred_bound)/2
-  D <- avg_FP_calc (obs, p, s, dist, m) 
+  #D <- avg_FP_calc (obs, p, s, dist, m) 
+  D = generate_data(obs,0,p,s, "Normal", 500, m)
+  #D <- mean(FP)
+  
   #D <- BinaryEntrp (xold, m) ## This is just the function we want to get to approximate root
   if (abs((D - desired_FAP)) < tol)
 	return(list(h_value = m, FP_value = D))
@@ -183,7 +199,8 @@ hBinarySearch <- function(l_pred_bound, u_pred_bound, pred_h, obs, p, dist = "No
       u = u
       m = (u+l)/2
     } 
-    D <- avg_FP_calc (obs, p, s, dist, m)   ## This is just the function we want to get to approximate root
+    D = generate_data(obs,0,p,s, "Normal", 500, m)
+    #D <- avg_FP_calc (obs, p, s, dist, m)   ## This is just the function we want to get to approximate root
     #write.csv(D, "D.csv")
 	if (abs((D-desired_FAP)) < tol)
       return(list(h_value = m, FP_value = D)) 
@@ -192,35 +209,24 @@ hBinarySearch <- function(l_pred_bound, u_pred_bound, pred_h, obs, p, dist = "No
   #stop ("Exceeded")
 }
 
-
-avg_FP_calc <- function(obs, p, s, dist, h1)
-{
-  FP <- rep(0, 500)
-  for (i in 1:500)
-  {
-    dataset = generate_data(obs, 0, p, s, dist, i)
-    rmdp_res = RMDP(dataset$data)
-    FP[i] = sum(rmdp_res > h1) / obs 
-  }
-  FP <- mean(FP)
-}
-
-
-
 ## Example
 dist = "Normal"
-obs = 50
-p = 50
-corr = 0.25
+obs = 10
+p = 2
+corr = 0.5
 lower = 0
-upper = 10
+upper = 25
 est = 5
 
-c = obs
-#write.csv(c, "c.csv")
-
 ## Example 
+start_time <- Sys.time()
 result <- hBinarySearch(lower, upper, est, obs, p,  dist, s = corr)
+end_time <- Sys.time()
+end_time - start_time
+
 result$h_value
 result$FP_value
 write.csv(result, "/home/martinwg/OCP/result.csv")
+
+
+
